99b095d5797c692529be018b89fb56f2a381a39d
Ganesh K Bhat <ganeshsurfs@gmail.com>
2025-04-04 16:26:12 +0530

    [COMMIT] adding tests for the package for all json manager functions

--- test/flatjson.test.js
+++ test/flatjson.test.js
@@ -178,3 +178,142 @@ describe('JsonManager', function () {
   });
 });
 
+// describe('JsonManager', () => {
+//   let jsonManager;
+
+//   beforeEach(() => {
+//     jsonManager = JsonManager();
+//   });
+
+//   describe('read', () => {
+//     it('should return the value of an existing key', () => {
+//       jsonManager.write('key1', 'value1');
+//       const result = jsonManager.read('key1');
+//       expect(result).to.equal('value1');
+//     });
+
+//     it('should return undefined for a non-existing key without createKey option', () => {
+//       const result = jsonManager.read('key2');
+//       expect(result).to.be.undefined;
+//     });
+
+//     it('should create a key with null value if createKey option is true', () => {
+//       const result = jsonManager.read('key3', { createKey: true });
+//       expect(result).to.be.null;
+//       expect(jsonManager.hasKey('key3')).to.be.true;
+//     });
+//   });
+
+//   describe('write', () => {
+//     it('should set a value for a key', () => {
+//       jsonManager.write('key1', 'value1');
+//       expect(jsonManager.read('key1')).to.equal('value1');
+//     });
+//   });
+
+//   describe('dump', () => {
+//     it('should return a shallow copy of the data object', () => {
+//       jsonManager.write('key1', 'value1');
+//       const dump = jsonManager.dump();
+//       expect(dump).to.deep.equal({ key1: 'value1' });
+//     });
+//   });
+
+//   describe('dumpKeys', () => {
+//     it('should search keys based on criteria', () => {
+//       jsonManager.write('key1', 'value1');
+//       jsonManager.write('key2', 'value2');
+//       const results = jsonManager.dumpKeys('key1');
+//       expect(results).to.deep.equal([{ key: 'key1', value: 'value1' }]);
+//     });
+//   });
+
+//   describe('dumpToFile', () => {
+//     it('should write the JSON object to a file', () => {
+//       const writeStub = sinon.stub(fs, 'writeFileSync');
+//       jsonManager.write('key1', 'value1');
+//       const result = jsonManager.dumpToFile(jsonManager.dump(), 'test.json');
+//       expect(result).to.be.true;
+//       expect(writeStub.calledOnce).to.be.true;
+//       writeStub.restore();
+//     });
+//   });
+
+//   describe('hasKey', () => {
+//     it('should return true if the key exists', () => {
+//       jsonManager.write('key1', 'value1');
+//       expect(jsonManager.hasKey('key1')).to.be.true;
+//     });
+
+//     it('should return false if the key does not exist', () => {
+//       expect(jsonManager.hasKey('key2')).to.be.false;
+//     });
+//   });
+
+//   describe('getKey', () => {
+//     it('should return the value of an existing key', () => {
+//       jsonManager.write('key1', 'value1');
+//       expect(jsonManager.getKey('key1')).to.equal('value1');
+//     });
+
+//     it('should return undefined for a non-existing key', () => {
+//       expect(jsonManager.getKey('key2')).to.be.undefined;
+//     });
+//   });
+
+//   describe('deleteKey', () => {
+//     it('should delete an existing key and return true', () => {
+//       jsonManager.write('key1', 'value1');
+//       const result = jsonManager.deleteKey('key1');
+//       expect(result).to.be.true;
+//       expect(jsonManager.hasKey('key1')).to.be.false;
+//     });
+
+//     it('should return false if the key does not exist', () => {
+//       const result = jsonManager.deleteKey('key2');
+//       expect(result).to.be.false;
+//     });
+//   });
+
+//   describe('init', () => {
+//     it('should initialize the data object with the provided object', () => {
+//       jsonManager.init({ key1: 'value1' });
+//       expect(jsonManager.dump()).to.deep.equal({ key1: 'value1' });
+//     });
+//   });
+
+//   describe('update', () => {
+//     it('should update the data object with the provided object', () => {
+//       jsonManager.init({ key1: 'value1' });
+//       jsonManager.update({ key2: 'value2' });
+//       expect(jsonManager.dump()).to.deep.equal({ key1: 'value1', key2: 'value2' });
+//     });
+//   });
+
+//   describe('search', () => {
+//     it('should return matching key-value pairs based on criteria', () => {
+//       jsonManager.write('key1', 'value1');
+//       jsonManager.write('key2', 'value2');
+//       const results = jsonManager.search('key1');
+//       expect(results).to.deep.equal([{ key: 'key1', value: 'value1' }]);
+//     });
+//   });
+
+//   describe('searchValue', () => {
+//     it('should return matching key-value pairs based on value criteria', () => {
+//       jsonManager.write('key1', 'value1');
+//       jsonManager.write('key2', 'value2');
+//       const results = jsonManager.searchValue('value1');
+//       expect(results).to.deep.equal([{ key: 'key1', value: 'value1' }]);
+//     });
+//   });
+
+//   describe('searchKeyValue', () => {
+//     it('should return matching key-value pairs based on key or value criteria', () => {
+//       jsonManager.write('key1', 'value1');
+//       jsonManager.write('key2', 'value2');
+//       const results = jsonManager.searchKeyValue('key1');
+//       expect(results).to.deep.equal([{ key: 'key1', value: 'value1' }]);
+//     });
+//   });
+// });
