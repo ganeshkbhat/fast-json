16e916f9895a61e42dcfb6b3a4ec08a86a0a77a2
Ganesh K Bhat <ganeshsurfs@gmail.com>
2025-11-26 19:58:40 +0530

    [COMMIT] Adding lock and flag for lock for read-write lock

---
+++ demos/demos.lock.js
@@ -0,0 +1,28 @@
+
+
+const JsonManager = require('../index').JsonManager; // Replace with the path to your implementation
+
+// Usage Example:
+const manager = new JsonManager();
+
+// init or clear data 
+manager.init({}); // inits or clears with a blank object
+manager.init(); // inits or clears with a blank object
+
+if (!manager.lock()) {
+    manager.setlock(true)
+    console.log("Filing a lock")
+}
+// Adding data
+manager.write('key1', 'value1');
+manager.write('key2', 'value2');
+manager.write('anotherKey', 'value3');
+manager.write('keyWithValue1', 'value1');
+
+if (!!manager.lock()) {
+    manager.droplock()
+    console.log("Dropping lock")
+}
+
+manager.write('anotherKey', 'value3');
+manager.write('keyWithValue1', 'value1');
